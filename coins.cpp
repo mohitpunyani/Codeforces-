f0a3727c1c6ea5d548e00dd86e88c312033912f5
MohitPunyani <mohitpunyani916@gmail.com>
2025-07-27 19:01:46 +0530

    Create coins.cpp

---
+++ coins.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        long long n, k;
+        cin >> n >> k;
+        if (n % 2 == 0 || (n - k) % 2 == 0) // we took y = 0 or y = 1
+            cout << "YES" << endl;
+        else
+            cout << "NO" << endl;
+    }
+    return 0;
+}
+ 
